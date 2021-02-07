220923d7e24eb5fe0f77d0447744c3b935f499c7
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-02-07 23:37:06 +0530

    algorithm done test failing

--- src/Dijkstra.cpp
+++ src/Dijkstra.cpp
@@ -10,9 +10,81 @@ Dijkstra::Dijkstra(Node* source, Node* destination) {
 Dijkstra::~Dijkstra() {
 }
 
-vector<Node*> Dijkstra::getShortestPath(){
+vector<Node*> Dijkstra::getShortestPath() {
+    this->sourceNode->setNodeCost(0);
+    this->transverseList.push_back(this->sourceNode);
     run();
+    fillShortestPath();
+    return this->shortestPath;
 }
-void Dijkstra::run(){
 
+void Dijkstra::run() {
+    bool reached = false;
+    while (true) {
+        Node* currentNode = getSmallestFromTransverseList();
+
+        auto edges = currentNode->getConnectedEdges();
+
+        for (int i = 0; i < edges.size(); i++) {
+            Edge* currentEdge = edges[i];
+
+            Node* otherNode = currentEdge->getOtherNode(currentNode);
+
+            int newCost = currentNode->getNodeCost() + currentEdge->getCost();
+
+            if (otherNode->isDestination()) {
+                otherNode->setNodeCost(newCost);
+                otherNode->setEdgeWithShortestPathToSource(currentEdge);
+                reached = true;
+                break;
+            }
+
+            if (otherNode->getNodeCost() > newCost) {
+                otherNode->setNodeCost(newCost);
+
+                otherNode->setTransversed(false);
+
+                otherNode->setEdgeWithShortestPathToSource(currentEdge);
+
+                addToTransverseList(otherNode);
+            }
+        }
+
+        if (reached) {
+            break;
+        }
+    }
+}
+
+Node* Dijkstra::getSmallestFromTransverseList() {
+    Node* smallest = this->transverseList[0];
+    int index = 0;
+    for (int i = 0; i < this->transverseList.size(); i++) {
+        auto currentNode = this->transverseList[i];
+        if (currentNode->getNodeCost() > smallest->getNodeCost()) {
+            smallest = currentNode;
+            index = i;
+        }
+    }
+
+    std::vector<Node*>::iterator it = this->transverseList.begin();
+    std::advance(it, index);
+    this->transverseList.erase(it);
+
+    return smallest;
+}
+
+void Dijkstra::fillShortestPath() {
+    Node* currentNode = destinationNode;
+    while (!currentNode->isSource()) {
+        shortestPath.push_back(currentNode);
+        auto edge = currentNode->getEdgeWithSortestPathToSource();
+        currentNode = edge->getOtherNode(currentNode);
+    }
+
+    shortestPath.push_back(this->sourceNode);
+}
+
+void Dijkstra::addToTransverseList(Node* node) {
+    this->transverseList.push_back(node);
 }
--- src/Dijkstra.hpp
+++ src/Dijkstra.hpp
@@ -1,14 +1,23 @@
 #ifndef DIJKSTRA_HPP
 #define DIJKSTRA_HPP
 
+#include <vector>
+
 #include "Edge.hpp"
 #include "Node.hpp"
+
+using namespace std;
 
 class Dijkstra {
    private:
     Node* sourceNode;
     Node* destinationNode;
+    vector<Node*> transverseList;
+    vector<Node*> shortestPath;
     void run();
+    Node* getSmallestFromTransverseList();
+    void addToTransverseList(Node* node);
+    void fillShortestPath();
 
    public:
     Dijkstra(Node* source, Node* destination);
