50f60d9f6a318a4d5f5639a9021705688b7edf7b
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-02-07 13:25:58 +0530

    circular dependencies

--- test/testEdge.cpp
+++ test/testEdge.cpp
@@ -7,10 +7,15 @@ using namespace std;
 TEST_CASE("Edge Constructor", "[Edge]") {
     shared_ptr<Node> firstNode = make_shared<Node>("A");
     shared_ptr<Node> secondNode = make_shared<Node>("B");
+    
     shared_ptr<Edge> edge = make_shared<Edge>(10, firstNode, secondNode);
     REQUIRE(edge->getCost() == 10);                     // edge should have a cost
+
+    //edge should have two nodes
     REQUIRE(edge->getFirstNode().lock() == firstNode);  // first node in edge
     REQUIRE(edge->getSecondNode().lock() == secondNode);  // second node in edge
+
+    //edge should return other node
     REQUIRE(edge->getOtherNode(firstNode).lock() == secondNode); // other node should be secondNode
-    REQUIRE(edge->getOtherNode(secondNode).lock() == firstNode); // other node should be secondNode
+    REQUIRE(edge->getOtherNode(secondNode).lock() == firstNode); // other node should be firstNode
 }
--- test/testNode.cpp
+++ test/testNode.cpp
@@ -1,10 +1,33 @@
 #include "catch_amalgamated.hpp"
-#include <memory>
+#include "Edge.hpp"
 #include "Node.hpp"
 
+using namespace std;
+
 TEST_CASE( "Node Constructor", "[Node]" ) {
-    std::shared_ptr<Node> node = std::make_shared<Node>("A");
+    shared_ptr<Node> node = make_shared<Node>("A");
     REQUIRE(node->getName() == "A");
     node->setCost(2);
     REQUIRE(node->getCost() == 2);
 }
+
+// TEST_CASE("multiple Edges In Node", "[Node Edges]"){
+//     shared_ptr<Node> node = make_shared<Node>("A");
+
+//     // node->add.Edge(make_shared<Edge>(10, node));
+// }
+
+// TEST_CASE("nodes having connecting edge"){
+    
+// }
+
+TEST_CASE("Node equal"){
+    shared_ptr<Node> node1a = make_shared<Node>("A");
+    shared_ptr<Node> node1b = make_shared<Node>("A");
+    shared_ptr<Node> node2 = make_shared<Node>("B");
+
+    REQUIRE(node1a == node1b);
+    REQUIRE(node2 != node1b);
+}
+
+
