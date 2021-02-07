e87f902a73f30d42e44541a63abeff9653820eda
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-02-07 21:09:57 +0530

    add Dijkstra files

---
+++ src/Dijkstra.hpp
@@ -0,0 +1,25 @@
+#ifndef DIJKSTRA_HPP
+#define DIJKSTRA_HPP
+
+#include "Edge.hpp"
+#include "Node.hpp"
+
+class Dijkstra {
+   private:
+    Node* sourceNode;
+    Node* destinationNode;
+    // run()
+
+   public:
+    Dijkstra(/* args */);
+    ~Dijkstra();
+    // getShortestpath()
+};
+
+Dijkstra::Dijkstra(/* args */) {
+}
+
+Dijkstra::~Dijkstra() {
+}
+
+#endif
---
+++ test/testDijkstra.cpp
@@ -0,0 +1,6 @@
+#include "catch_amalgamated.hpp"
+#include "Dijkstra.hpp"
+
+// TEST_CASE("Dijkstra Constrcutor"){
+//     Node* 
+// }
