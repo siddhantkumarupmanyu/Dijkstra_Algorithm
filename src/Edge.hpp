f5946b1d3fd71f76be7bca8f22470fb150cca5f5
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-02-07 12:49:33 +0530

    edge done

--- src/Edge.hpp
+++ src/Edge.hpp
@@ -2,19 +2,22 @@
 #define EDGE_HPP
 
 #include <memory>
+
 #include "Node.hpp"
 
-class Edge
-{
-private:
+class Edge {
+   private:
     int cost;
     std::weak_ptr<Node> firstNode;
     std::weak_ptr<Node> secondNode;
-public:
-    Edge(int cost);
+
+   public:
+    Edge(int cost, std::weak_ptr<Node> firstNode, std::weak_ptr<Node> secondNode);
     ~Edge();
     int getCost();
     std::weak_ptr<Node> getFirstNode();
+    std::weak_ptr<Node> getSecondNode();
+    std::weak_ptr<Node> getOtherNode(const std::shared_ptr<Node> node);
 };
 
 #endif
