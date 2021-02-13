c659bc5ac5673689792d65dd261aa371b5754567
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-02-13 16:49:48 +0530

    intermediate commit

--- spikes/TempFile.cpp
+++ spikes/TempFile.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+// #include <filesystem>
 
 using namespace std;
+// namespace fs = std::filesystem;
 
 int main(){
-    cout << "hellow" << endl;
+    // cout << "Temp directory is " << fs::temp_directory_path() << '\n';
 }
